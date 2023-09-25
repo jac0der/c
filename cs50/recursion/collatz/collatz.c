228b29701b5d821f4ec07914d305e7b2d6a7cedb
jacoder <mantle@vivaldi.net>
2023-09-24 23:43:31 -0500

    COLLATZ
    Add comments to function definitions.

--- cs50/recursion/collatz/collatz.c
+++ cs50/recursion/collatz/collatz.c
@@ -4,6 +4,8 @@
 /*
     Recursive program that calculates how many steps it takes 
     to get to 1 if you start from n and recurse as indicated in README_COLLATZ.
+    @datetime:: September 24, 2023 8:11 am (UTC-5)
+    @author:: jac0der
 */
 
 // function prototype definitions
@@ -21,11 +23,20 @@ int main(void)
         return 1;
     }
 
+    // print out number of steps.
     printf("Steps count is: %i", collatz(number));
 }
 
 /*
-    Function to implement the Collatz Conjecture algorithm
+    Function to implement the Collatz Conjecture algorithm -
+    which is to calculate the number of steps taken
+    to get to 1, given a number n, using the algorithm
+    for the collatz sequence, as outlined in the 
+    README_COLLATZ read me file.
+    @input:: value enetered by user
+    @output:: number of steps taking to get to 1, from 
+              a number n.
+"""
 */
 int collatz(int number)
 {
@@ -50,16 +61,16 @@ int collatz(int number)
 }
 
 /*
-    Function to determine if a number is even or odd.
+    Function to determine if an integer is even or odd.
+    Modulus operator wqas used to get the modulus of number / 2.  If there
+    is no remainder, it means number is exactly
+    divissible by 2, Thus number is even, otherwise odd.
+    @input:: number entered by user.
+    @output:: True if number is Even, otherwise False.
 */
 bool isEven(int number)
 {
-    /*
-        get modulus of number / 2.  If there
-        is no remainder, it means number is exactly 
-        divissible by 2, Thus number is even, otherwise odd.
-    */ 
-    if((number % 2) == 0)
+    if( (number % 2) == 0 )
     {
         return true;
     }
