d52fe89bf8cb1323db0b84245bd14a2d72d9e0e4
jacoder <mantle@vivaldi.net>
2023-09-26 23:27:11 -0500

    LINEAR SEARCH Add comments in code explaining its implementation.

--- cs50/algorithms/searching/linear_search/linear_search.c
+++ cs50/algorithms/searching/linear_search/linear_search.c
@@ -2,10 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+    Linear search implementation to find an element in a list
+    @datetime:: September 25, 2023 11:16 am (UTC-5)
+    @lastModified:: September 26, 2023 5:09 am (UTC-5)
+    @author:: jac0der
+"""
+*/
+
+// define list maximum elements
 const int LISTLENGTH = 15;
 
+// accept command line arguments
 int main(int argc, string argv[])
 {
+    // ensure 1 argument is passed at commannd prompt
     if(argc == 1 || argc > 2)
     {
         printf("Invalid input for %s\n", argv[0]);
@@ -15,6 +26,7 @@ int main(int argc, string argv[])
     int search_list[] = {11, 23, 8, 14, 30, 9, 6, 17, 22,
                 28, 25, 15, 7, 10, 19};
     
+    // loop through each element until target is found
     for(int i = 0; i < LISTLENGTH; i++)
     {
         if(search_list[i] == atoi(argv[1]))
